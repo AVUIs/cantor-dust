1249f86a8f32658df71591dc3d8aee9e87856cfe
Berkan Eskikaya <berkan.eskikaya@gmail.com>
2015-07-18 18:54:32 +0100

    first sound

--- src/main.js
+++ src/main.js
@@ -1,4 +1,45 @@
 'use strict';
 
 var worker = new Worker('worker/fractal.js');
+worker.onmessage = function(e) {
+    play(e.data);
+}
+    
 window.worker = worker;
+
+// modified from http://mdn.github.io/audio-buffer/
+var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+var play = function (raw_data) {
+
+    var data_length = raw_data.length;
+    
+    // Stereo
+    var channels = 2;
+    // Create an empty two second stereo buffer at the
+    // sample rate of the AudioContext
+    var frameCount = audioCtx.sampleRate * 20.0;
+
+    var myArrayBuffer = audioCtx.createBuffer(channels, frameCount, audioCtx.sampleRate);
+    
+    for (var channel = 0; channel < channels; channel++) {
+	// This gives us the actual array that contains the data
+	var nowBuffering = myArrayBuffer.getChannelData(channel);
+	for (var i = 0; i < frameCount; i++) {
+	    // Math.random() is in [0; 1.0]
+	    // audio needs to be in [-1.0; 1.0]
+	    nowBuffering[i] = raw_data[i % data_length]
+	}
+    }
+
+    // Get an AudioBufferSourceNode.
+    // This is the AudioNode to use when we want to play an AudioBuffer
+    var source = audioCtx.createBufferSource();
+    // set the buffer in the AudioBufferSourceNode
+    source.buffer = myArrayBuffer;
+    // connect the AudioBufferSourceNode to the
+    // destination so we can hear the sound
+    source.connect(audioCtx.destination);
+    // start the source playing
+    source.start();
+}
+window.play = play;
--- worker/fractal.js
+++ worker/fractal.js
@@ -2,10 +2,10 @@
 
 var arr = new Float64Array(8);
 
-onmessage = function(message) {
+onmessage = function(e) {
   // var data = message.data;
-
-  cantor();
+    var data = cantor([0.33,1,0.75,0.5],7);
+    postMessage(data);
 };
 
 // 1.0
@@ -15,7 +15,7 @@ onmessage = function(message) {
 function cantor(pattern, iterations) {
   var arr = [1];
   pattern    = pattern || [1, 0.5, 1];
-  iterations = iterations || 5;
+  iterations = iterations || 10;
 
   while (iterations--) {
     arr = vecMult(pattern, arr);
