4d9122bb7c7c50eaad65320f4b9f7635daa0b509
Louis Pilfold <louis@lpil.uk>
2015-07-19 14:08:55 +0100

    Remove dead code

--- src/midi.js
+++ src/midi.js
@@ -2,10 +2,6 @@
 
 var midiAccess;
 
-var midicontroller = {},
-    // TODO: remove
-    launchpad = midicontroller;
-
 navigator.requestMIDIAccess().then(
   e => { midiAccess = e; },
   _ => { console.log('Failed to get midi access.'); }
@@ -30,47 +26,4 @@ function getDevice(cb, nameRegex) {
   cb(input, output);
 }
 
-export default { getDevice, initMidiController };
-
-
-
-// Use the launchpad rotated 90 degrees anticlockwise
-
-// Decimal Colour Brightness
-// 12      Off    Off
-// 13      Red    Low
-// 15      Red    Full
-// 29      Amber  Low
-// 63      Amber  Full
-// 62      Yellow Full
-// 28      Green  Low
-// 60      Green  Full 
-
-//var launchpad = {};
-
-function initMidiController() {
-  getDevice(
-    function(inp, out)  {
-      inp.onmidimessage = printMidiMessage;
-      midicontroller = { input: inp, output: out };
-    },
-    /^Launch(pad| Control)/
-  );
-}
-
-
-
-
-// Grid starts bottom left
-function midiNoteToGrid(note) {
-  var x = Math.floor(note / 16),
-      y = note % 16;
-  return [x, y];
-}
-
-function printMidiMessage(e) {
-  // var cell = midiNoteToGrid(e.data[1]);
-  // launchpad.output.send([144, e.data[1], 63]);
-  // setTimeout(() => { launchpad.output.send([144, e.data[1], 12]); }, 500);
-  console.log('Got MIDI:', e.data);
-}
+export default { getDevice };
