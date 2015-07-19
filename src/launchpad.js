462b5ee6d347389ad0a90a6bdce6b3dbd650c3a2
Louis Pilfold <louis@lpil.uk>
2015-07-19 16:48:09 +0100

    Lots of Launchpad fun

--- src/launchpad.js
+++ src/launchpad.js
@@ -12,19 +12,61 @@ import midi from 'midi';
 // 63      Amber  Full
 // 62      Yellow Full
 // 28      Green  Low
-// 60      Green  Full 
+// 60      Green  Full
 
 var launchpad = {};
 
 function initLaunchpad() {
   midi.getDevice(
-    function(inp, out)  {
-      inp.onmidimessage = printMidiMessage;
-      launchpad = { input: inp, output: out };
+    (input, output) => {
+      input.onmidimessage = recieveMIDIMessage;
+      launchpad = { input, output };
     },
     /^Launchpad/
   );
 }
+
+function isSideButton(note) {
+  return !(note <= 104 || note >= 111);
+}
+function isTopButton(note) {
+  return (
+    note === 8   ||
+    note === 24  ||
+    note === 40  ||
+    note === 56  ||
+    note === 72  ||
+    note === 88  ||
+    note === 104 ||
+    note === 120
+  );
+}
+function isGrid(note) {
+  return !(isTopButton(note) || isSideButton(note));
+}
+
+function lightOn(note) {
+  launchpad.output.send([144, note, 63]);
+}
+function lightOff(note) {
+  launchpad.output.send([144, note, 12]);
+}
+function lightFlash(note, duration) {
+  lightOn(note);
+  setTimeout(() => { lightOff(note); }, duration || 500);
+}
+function lightColumn(col, num) {
+  var colOffset = gridToMidiNote(col, 0),
+      i = num + 1;
+  lightColumnOff(col);
+  while (i--) { lightOn(colOffset + i); }
+}
+function lightColumnOff(col) {
+  var colOffset = gridToMidiNote(col, 0),
+      i = 8;
+  while (i--) { lightOff(colOffset + i); }
+}
+
 
 // Grid starts bottom left
 function midiNoteToGrid(note) {
@@ -32,12 +74,19 @@ function midiNoteToGrid(note) {
       y = note % 16;
   return [x, y];
 }
+function gridToMidiNote(x, y) {
+  return x * 16 + y;
+}
 
-function printMidiMessage(e) {
-  var cell = midiNoteToGrid(e.data[1]);
-  launchpad.output.send([144, e.data[1], 63]);
-  setTimeout(() => { launchpad.output.send([144, e.data[1], 12]); }, 500);
-  console.log('Got MIDI:', cell);
+
+function recieveMIDIMessage(e) {
+  var note = e.data[1],
+      x,
+      y;
+  if (isGrid(note)) {
+    [x, y] = midiNoteToGrid(note);
+    lightColumn(x, y);
+  }
 }
 
 export default initLaunchpad;
