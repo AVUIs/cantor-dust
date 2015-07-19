9605d07d574edbf25548e9979ea3c46530d99856
Louis Pilfold <louis@lpil.uk>
2015-07-19 14:22:45 +0100

    Wavetables are modified while running

--- src/audio.js
+++ src/audio.js
@@ -2,60 +2,45 @@
 
 var audioCtxConstructor = (window.AudioContext || window.webkitAudioContext),
     audioCtx = new audioCtxConstructor(),
+    nSamples = Math.pow(2, 19),
     sources  = [],
+    buffers  = [],
     fractal  = [0],
     i = 8;
 
-function play(i, rawData) {
-  var source = audioCtx.createBufferSource(),
-      oldSrc = sources[i],
-      frames = rawData.length,
-      buffer = audioCtx.createBuffer(2, frames, audioCtx.sampleRate),
-      left   = buffer.getChannelData(0),
-      right  = buffer.getChannelData(1);
-
-  // Load the left and right channels with our audio data
-  while (frames--) {
-    left[frames]  = rawData[frames];
-    right[frames] = rawData[frames];
+(function() {
+  var source, buffer, left, right;
+  while (i--) {
+    source = audioCtx.createBufferSource();
+    buffer = audioCtx.createBuffer(2, nSamples, audioCtx.sampleRate);
+    left   = buffer.getChannelData(0);
+    right  = buffer.getChannelData(1);
+    buffers[i] = [left, right];
+    source.loop   = true;
+    source.buffer = buffer;
+    source.connect(audioCtx.destination);
+    source.start();
   }
-  if (oldSrc) {
-    oldSrc.stop();
-  }
-
-  // Play our buffer
-  source.loop   = true;
-  source.buffer = buffer;
-  source.connect(audioCtx.destination);
-  source.start();
-  sources[i] = source;
-  console.log("done");
-}
+}());
 
 
-function generate(pattern, iterations) {
-  console.log('Generating fractal...');
+function generate(i, pattern, iterations) {
   var worker = new Worker('worker/fractal.js');
   worker.onmessage = function(e) {
-    fractal = e.data;
-    console.log('Wavetable ready!');
+    console.log('Fractal generated');
+    load(i, e.data);
   };
   worker.postMessage([pattern, iterations]);
 }
 
-// Swaps the waiting wavetable for the playing wavetable for source `i`
-// Because it swaps you can effectively undo your decision by running it again
-function load(i) {
-  var previous,
-      next   = fractal,
-      source = sources[i];
-  if (source) {
-    previous = source.buffer.getChannelData(0);
-    fractal  = previous;
+function load(num, frames) {
+  var [left, right] = buffers[num],
+      framesLen = frames.length,
+      len, i;
+  for (i = 0, len = left.length; i < len; i++) {
+    left[i]  = frames[i % framesLen];
+    right[i] = frames[i % framesLen];
   }
-  play(i, next);
 }
 
-window.sources = sources;
-
-export default { generate, load };
+export default { generate };
