d168f3f5db0162e4e8455b6ee1112e24d6192356
Louis Pilfold <louis@lpil.uk>
2015-07-19 12:48:03 +0100

    Split out into additional modules

--- src/main.js
+++ src/main.js
@@ -4,59 +4,11 @@ import midi from 'midi';
 window.midi = midi;
 
 
-var audioCtxConstructor = (window.AudioContext || window.webkitAudioContext),
-    audioCtx = new audioCtxConstructor(),
-    sources  = [],
-    fractal  = [0],
-    i = 8;
-
-function play(i, rawData) {
-  var source = audioCtx.createBufferSource(),
-      oldSrc = sources[i],
-      frames = rawData.length,
-      buffer = audioCtx.createBuffer(2, frames, audioCtx.sampleRate),
-      left   = buffer.getChannelData(0),
-      right  = buffer.getChannelData(1);
-
-  // Load the left and right channels with our audio data
-  while (frames--) {
-    left[frames]  = rawData[frames];
-    right[frames] = rawData[frames];
-  }
-  if (oldSrc) {
-    oldSrc.stop();
-  }
-
-  // Play our buffer
-  source.loop   = true;
-  source.buffer = buffer;
-  source.connect(audioCtx.destination);
-  source.start();
-  sources[i] = source;
-}
+import initLaunchpad from 'launchpad';
+window.initLaunchpad = initLaunchpad;
 
 
-window.generate = function makeNoise(pattern, iterations) {
-  console.log('Generating fractal...');
-  var worker = new Worker('worker/fractal.js');
-  worker.onmessage = function(e) {
-    fractal = e.data;
-    console.log('Wavetable ready!');
-  };
-  worker.postMessage([pattern, iterations]);
-};
-
-window.sources = sources;
-
-// Swaps the waiting wavetable for the playing wavetable for source `i`
-// Because it swaps you can effectively undo your decision by running it again
-window.load = function load(i) {
-  var previous,
-      next   = fractal,
-      source = sources[i];
-  if (source) {
-    previous = source.buffer.getChannelData(0);
-    fractal  = previous;
-  }
-  play(i, next);
-};
+import audio from 'audio';
+window.audio = audio;
+window.load     = audio.load;
+window.generate = audio.generate;
--- src/midi.js
+++ src/midi.js
@@ -2,9 +2,6 @@
 
 var midiAccess;
 
-// TODO: remove
-var launchpad = {};
-
 navigator.requestMIDIAccess().then(
   e => { midiAccess = e; },
   _ => { console.log('Failed to get midi access.'); }
@@ -29,44 +26,4 @@ function getDevice(cb, nameRegex) {
   cb(input, output);
 }
 
-export default { getDevice, initLaunchpad };
-
-
-
-// Use the launchpad rotated 90 degrees anticlockwise
-
-// Decimal Colour Brightness
-// 12      Off    Off
-// 13      Red    Low
-// 15      Red    Full
-// 29      Amber  Low
-// 63      Amber  Full
-// 62      Yellow Full
-// 28      Green  Low
-// 60      Green  Full 
-
-var launchpad = {};
-
-function initLaunchpad() {
-  getDevice(
-    function(inp, out)  {
-      inp.onmidimessage = printMidiMessage;
-      launchpad = { input: inp, output: out };
-    },
-    /^Launchpad/
-  );
-}
-
-// Grid starts bottom left
-function midiNoteToGrid(note) {
-  var x = Math.floor(note / 16),
-      y = note % 16;
-  return [x, y];
-}
-
-function printMidiMessage(e) {
-  var cell = midiNoteToGrid(e.data[1]);
-  launchpad.output.send([144, e.data[1], 63]);
-  setTimeout(() => { launchpad.output.send([144, e.data[1], 12]); }, 500);
-  console.log('Got MIDI:', cell);
-}
+export default { getDevice };
