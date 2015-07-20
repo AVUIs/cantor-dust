6d0e857ce1986354a576947c882097ec5618baba
Louis Pilfold <louis@lpil.uk>
2015-07-20 07:55:12 +0100

    Refactor audio.js

--- src/audio.js
+++ src/audio.js
@@ -1,122 +1,58 @@
 'use strict';
 
-var audioCtxConstructor = (window.AudioContext || window.webkitAudioContext),
-    audioCtx = new audioCtxConstructor(),
-    nSamples = Math.pow(2, 19),
-    sources  = [],
-    buffers  = [],
-    jsNodes = [],
-    analysers = [],
-    controls = [],
-    fractal  = [0],
-    nsynths = 8;
+var numSynths = 8,
+    audioCtxConstructor = (window.AudioContext || window.webkitAudioContext),
+    audioCtx   = new audioCtxConstructor(),
+    numSamples = Math.pow(2, 19),
+    synths     = Array.apply(null, { length: numSynths });
 
-
-(function() {
-    var source, buffer, left, right;
-    var i = nsynths;
-  while (i--) {
-    source = audioCtx.createBufferSource();
-    buffer = audioCtx.createBuffer(2, nSamples, audioCtx.sampleRate);
-    left   = buffer.getChannelData(0);
-    right  = buffer.getChannelData(1);
-    buffers[i] = [left, right];
+class CantorSynth {
+  constructor() {
+    var source = audioCtx.createBufferSource(),
+        buffer = audioCtx.createBuffer(2, numSamples, audioCtx.sampleRate);
+    this.channels = {};
+    this.channels.left  = buffer.getChannelData(0);
+    this.channels.right = buffer.getChannelData(1);
+    this.configData = { pattern: [0], iterations: 1 };
     source.loop   = true;
     source.buffer = buffer;
     source.connect(audioCtx.destination);
     source.start();
-
-    sources[i] = source;  
-      
-    controls[i] = {iterations: 0, pattern: []};
   }
-}());
 
+  update(configData, wavetable) {
+    this.configData = configData;
+    this.bufferData = wavetable;
+  }
+
+  set bufferData(wavetable) {
+    loadWavetables(wavetable, this.channels.left, this.channels.right);
+  }
+  get bufferData() {
+    return [this.channels.left, this.channels.right];
+  }
+}
+
+function loadWavetables(samples, left, right) {
+  var numFrames = samples.length,
+      len, i;
+  for (i = 0, len = left.length; i < len; i++) {
+    left[i]  = samples[i % numFrames];
+    right[i] = samples[i % numFrames];
+  }
+}
 
 function generate(i, pattern, iterations) {
   var worker = new Worker('worker/fractal.js');
   worker.onmessage = function(e) {
+    var config = { pattern, iterations };
+    synths[i].update(config, e.data);
     console.log('Fractal generated');
-    load(i, e.data);
   };
-    controls[i].pattern = pattern;
-    controls[i].iterations = iterations;
   worker.postMessage([pattern, iterations]);
 }
 
-function load(num, frames) {
-  var [left, right] = buffers[num],
-      framesLen = frames.length,
-      len, i;
-  for (i = 0, len = left.length; i < len; i++) {
-    left[i]  = frames[i % framesLen];
-    right[i] = frames[i % framesLen];
-  }
-}
 
+synths = synths.map(() => new CantorSynth());
 
-
-// http://www.smartjava.org/content/exploring-html5-web-audio-visualizing-sound
-
-(function setupAudioNodes() {
-
-    for (var i=0; i<nsynths; i++) {
-	// setup a javascript node
-	var javascriptNode = audioCtx.createScriptProcessor(2048, 1, 1);
-	// connect to destination, else it isn't called
-	javascriptNode.connect(audioCtx.destination);
-
-	jsNodes[i] = javascriptNode;
-
-	
-	// setup an analyzer
-	var analyser = audioCtx.createAnalyser();
-	analyser.smoothingTimeConstant = 0.3;
-	analyser.fftSize = 512;
-
-	analysers[i] = analyser;
-
-	sources[i].connect(analysers[i]);
-	analysers[i].connect(jsNodes[i]);
-
-	var canvasid = "#c"+i;
-	var canvas = document.querySelector(canvasid);
-	var ctx = canvas.getContext("2d");
-	
-	(function(ctx, analyser) {
-	    javascriptNode.onaudioprocess = function() {
-
-	   
-		// get the average for the first channel
-		// var array =  new Uint8Array(analyser.frequencyBinCount);
-		// analyser.getByteFrequencyData(array);
-
-		var array =  new Uint8Array(analyser.fftSize);
-		analyser.getByteTimeDomainData(array);
-	
-		ctx.fillStyle = "rgba(255,0,0,0.05)";
-		
-		// clear the current state
-//		ctx.clearRect(0, 0, 1000, 325);
-		
-		// set the fill style
-//		ctx.fillStyle = "#ff0000";
-		drawSpectrum(array,ctx);
-
-	    }
-	})(ctx, analyser);
-	
-    }
-    
-})();
-
-
-function drawSpectrum(array,ctx) {
-    for ( var i = 0; i < (array.length); i++ ){
-        var value = array[i];
-        ctx.fillRect(i*5,325-value,3,325);
-    }
-};
-
-
-export default { generate, controls };
+export default { generate, synths, numSamples };
