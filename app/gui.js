40ea4249704129e0ad68f05344f3d9695f6ec5b0
Louis Pilfold <louis@lpil.uk>
2015-07-25 16:26:27 +0100

    Init the canvas

--- app/gui.js
+++ app/gui.js
@@ -1,7 +1,30 @@
 'use strict';
 
+var botCanvas = document.querySelector('canvas.bottom'),
+    topCanvas = document.querySelector('canvas.top'),
+    botCtx = botCanvas.getContext('2d'),
+    topCtx = topCanvas.getContext('2d');
+
+
 function update() {
   console.log('Updating GUI');
 }
+
+function resizeCanvas() {
+  botCanvas.width  = window.innerWidth;
+  botCanvas.height = window.innerHeight - 4;
+  topCanvas.width  = window.innerWidth;
+  topCanvas.height = window.innerHeight - 4;
+}
+
+window.onresize = resizeCanvas;
+resizeCanvas();
+
+setTimeout(function() {
+  botCtx.fillStyle = '#fff';
+  botCtx.fillRect(30, 15, 150, 100);
+  topCtx.fillStyle = 'hotpink';
+  topCtx.fillRect(50, 25, 150, 100);
+}, 200);
 
 export default { update };
