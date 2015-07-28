7c19401461334365a970d864bf2def1472e032c9
Louis Pilfold <louis@lpil.uk>
2015-07-28 18:59:51 +0100

    Can plot sliders

--- app/gui.js
+++ app/gui.js
@@ -8,7 +8,7 @@ var canvas = document.querySelector('canvas'),
     segmentH = window.innerHeight / 8;
 
 function dim(i) {
-  return { x: 0, y: i * segmentH, w: window.innerWidth - 200, h: segmentH };
+  return { x: 0, y: i * segmentH, w: window.innerWidth - 150, h: segmentH };
 }
 
 function updateCantor(i, cantorArr) {
@@ -18,13 +18,34 @@ function updateCantor(i, cantorArr) {
 function updateIterations(i, iterations) {
   var y = i * segmentH + 5,
       x = window.innerWidth - 5;
-  ctx.clearRect(x - 30, y, 30, 22);
+  ctx.clearRect(x - 25, y, 25, 22);
   ctx.textBaseline = 'hanging';
   ctx.textAlign    = 'right';
   ctx.fillStyle    = 'grey';
   ctx.font         = '22px monospace';
   ctx.fillText(iterations, x, y);
 }
+
+// Sorry...
+function updateSliders(n, params) {
+  var i = params.length,
+      x = Math.ceil(window.innerWidth - 70),
+      y = n * segmentH,
+      w = 95,
+      h = segmentH,
+      sw = Math.floor(w / i),
+      sh;
+  ctx.fillStyle = 'black';
+  ctx.fillRect(x - w + sw, y, w, segmentH);
+  ctx.fillStyle = 'grey';
+  while (i--) {
+    sh = 0 - (h * params[i]);
+    ctx.fillRect(x + 1, y + h, sw - 1, sh);
+    x -= sw;
+  }
+}
+
+window.updateSliders = updateSliders;
 
 function updateAll() {
   var stateI,
